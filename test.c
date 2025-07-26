3d0423247d7f2a40437de78584f34a4b668f7eb2
Hazem Essam <hazem3ssam20@gmail.com>
2025-07-26 10:59:58 +0300

    testing doxygen

--- test.c
+++ test.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 
 /**
- * @brief this is a modification on the repo to test integrating with VScode
- * @attention just testing a thoery
+ * @brief Just wanted to test Doxygen
+ * 
+ * @param a int
+ * @return int 
  */
+int testing_dox(int a)
+{
+	printf("%d",a);
+	return a++;
+}
 
 int main()
 {
+	testing_dox(1);
 	printf("\nWelcome To My REPO\n");
 }
 
